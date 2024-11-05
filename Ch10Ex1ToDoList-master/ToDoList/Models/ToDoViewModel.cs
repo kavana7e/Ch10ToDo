927dc078b05053b00049ee7bf45a1de8c788d23c
kavana7e <kavana7@mail.nmc.edu>
2024-11-05 12:22:25 -0500

    Add project files.

---
+++ Ch10Ex1ToDoList-master/ToDoList/Models/ToDoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ToDoList.Models
+{
+    public class ToDoViewModel
+    {
+        [ValidateNever]
+        public Filters Filters { get; set; } = null!;
+        [ValidateNever]
+        public List<Status> Statuses { get; set; } = null!;
+        [ValidateNever]
+        public List<Category> Categories { get; set; } = null!;
+        [ValidateNever]
+        public Dictionary<string, string> DueFilters { get; set; } = null!;
+        [ValidateNever]
+        public List<ToDo> Tasks { get; set; } = null!;
+        [ValidateNever]
+        public ToDo CurrentTask { get; set; } = null!; //adding task
+    }
+}
